ad131fdbdd92cf6c3e4bfa16cd5abcaffbee7223
NorthWestTrees <northwesttreesgaming@gmail.com>
2023-05-30 11:57:32 -0700

    1.4.0 Final

---
+++ src/main/java/net/nwtg/myeconomy/procedures/SpawnSetWorldSpawnProcedure.java
@@ -0,0 +1,67 @@
+package net.nwtg.myeconomy.procedures;
+
+import net.nwtg.myeconomy.network.MyeconomyModVariables;
+
+import net.minecraftforge.fml.loading.FMLPaths;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
+
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.File;
+import java.io.BufferedReader;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.Gson;
+
+public class SpawnSetWorldSpawnProcedure {
+	public static void execute(LevelAccessor world, Entity entity) {
+		if (entity == null)
+			return;
+		File file = new File("");
+		com.google.gson.JsonObject mainObject = new com.google.gson.JsonObject();
+		if (!world.isClientSide()) {
+			file = new File((FMLPaths.GAMEDIR.get().toString() + "/config/myeconomy/worlds"), File.separator + (MyeconomyModVariables.WorldVariables.get(world).worldName + ".json"));
+			if (file.exists()) {
+				{
+					try {
+						BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+						StringBuilder jsonstringbuilder = new StringBuilder();
+						String line;
+						while ((line = bufferedReader.readLine()) != null) {
+							jsonstringbuilder.append(line);
+						}
+						bufferedReader.close();
+						mainObject = new Gson().fromJson(jsonstringbuilder.toString(), com.google.gson.JsonObject.class);
+						mainObject.addProperty("home_x", (Math.floor(entity.getX()) + 0.5));
+						mainObject.addProperty("home_y", (Math.floor(entity.getY()) + 0.5));
+						mainObject.addProperty("home_z", (Math.floor(entity.getZ()) + 0.5));
+						mainObject.addProperty("home_world", MyeconomyModVariables.WorldVariables.get(world).worldNamespace);
+						mainObject.addProperty("has_home", (true));
+						{
+							Gson mainGSONBuilderVariable = new GsonBuilder().setPrettyPrinting().create();
+							try {
+								FileWriter fileWriter = new FileWriter(file);
+								fileWriter.write(mainGSONBuilderVariable.toJson(mainObject));
+								fileWriter.close();
+							} catch (IOException exception) {
+								exception.printStackTrace();
+							}
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				if (entity instanceof Player _player && !_player.level.isClientSide())
+					_player.displayClientMessage(Component.literal((Component.translatable("msg.myeconomy.spawn_set_world_spawn.success").getString())), (false));
+			} else {
+				if (entity instanceof Player _player && !_player.level.isClientSide())
+					_player.displayClientMessage(Component.literal((Component.translatable("msg.myeconomy.spawn_tp_player.error1").getString())), (false));
+			}
+		}
+	}
+}
